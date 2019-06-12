2e25aed0c486a676db945f54eb0ab522e3c12bd6
Michael D <miketdennison@gmail.com>
2019-06-12 07:10:37 -0500

    added keypress functionality and button clicking

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,41 @@
 var gamePattern = [];
 var userClickedPattern = []
 var buttonColors = ["red", "blue", "green", "yellow"];
+var level = 0;
 
 $(".btn").on("click", function () {
     userChosenColor = $(this).attr("id");
+    playSound(userChosenColor)
+    animatePress(userChosenColor);
+});
+
+$(document).one("keypress", function() {
+    $("#level-title").text(`Level ${level}`);
+    nextSequence();
 });
 
 function nextSequence() {
+    level++;
+    $("#level-title").text("Level " + level);
+    
     var randomNumber = Math.floor((Math.random() * 4));
     var randomChosenColor = buttonColors[randomNumber];
+    
     gamePattern.push(randomChosenColor);
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+}
 
-    console.log("assets/sounds/" + randomChosenColor + ".mp3");
-    var audio = new Audio("assets/sounds/" + randomChosenColor + ".mp3");
+function playSound(name) {
+    var audio = new Audio("assets/sounds/" + name + ".mp3");
     audio.play();
 }
 
-nextSequence();
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
 
